ab81cf40dfc495e865298a9f4cef7f7f2af8f303
Antoine Meyer <antoine.meyer@univ-eiffel.fr>
2023-02-13 12:43:25 +0100

    config pour tp3 (pas encore de tests)

---
+++ tp3/tests/Tp3Tests.hs
@@ -0,0 +1,13 @@
+module Tp3Tests where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck as QC
+import Test.Tasty.SmallCheck as SC
+import Test.SmallCheck.Series as SCS
+
+import Tp3
+
+tests = testGroup "Tests for TP 3"
+    [ 
+    ]
---
+++ tp3/tests/test.hs
@@ -0,0 +1,4 @@
+import Tp3Tests as Tests
+import Test.Tasty
+
+main = defaultMain Tests.tests
